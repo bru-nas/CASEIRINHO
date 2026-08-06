03d81040976ce28baea2a27c981b2567638ac5a4
MONSTER <miztella@gmail.com>
2026-08-06 11:46:02 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+/* ============================================================
+   Cole aqui a configuração do seu projeto Firebase
+   (Console Firebase > Configurações do projeto > Seus apps > SDK setup)
+   Enquanto não configurar, o app funciona com armazenamento local
+   no navegador (útil pra testar, mas não sincroniza entre dispositivos).
+   ============================================================ */
+const firebaseConfig = {
+  apiKey: "AIzaSyCmQhmED2nVC78jDiqmMnEgOHtCQPHHP9A",
+  authDomain: "caseirinho-51122.firebaseapp.com",
+  databaseURL: "https://caseirinho-51122-default-rtdb.firebaseio.com",
+  projectId: "caseirinho-51122",
+  storageBucket: "caseirinho-51122.firebasestorage.app",
+  messagingSenderId: "11068035844",
+  appId: "1:11068035844:web:02c38e0673fa87084d3651",
+};
